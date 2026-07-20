14a211a9f6fbdfb87b50e1255737c5b6af96cd4c
Dylan Sharp <dgstatic@gmail.com>
2026-07-19 21:18:31 -0600

    New Measure Shapes (#35)
    * measure cone
    
    * measure area
    
    * viewMode not used while measuring
    
    * prettier
    
    * unused import
    
    * 53 degrees instead of 30

---
+++ web/src/lib/components/MeasureToolOptionMenu.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { type MeasureTool } from "$lib/game/level-editor";
+  import StyledCard from "$lib/components/StyledCard.svelte";
+  import StyledButton from "./StyledButton.svelte";
+
+  let { measure = $bindable({ type: "measure", start: null, shape: "line" } as MeasureTool) } =
+    $props();
+
+  const measureShapes = $derived([
+    {
+      label: "Line",
+      selected: measure.shape === "line",
+      tool: { type: "measure", start: null, shape: "line" },
+    },
+    {
+      label: "Cone",
+      selected: measure.shape === "cone",
+      tool: { type: "measure", start: null, shape: "cone" },
+    },
+    {
+      label: "Area",
+      selected: measure.shape === "area",
+      tool: { type: "measure", start: null, shape: "area" },
+    },
+  ] as const);
+</script>
+
+<StyledCard class="grid gap-3 px-4 py-3 pointer-events-auto">
+  <div class="flex gap-2">
+    {#each measureShapes as measureShape, i (i)}
+      <StyledButton
+        mode={measureShape.selected ? "primary" : "outline"}
+        onclick={() => {
+          measure = measureShape.tool;
+        }}
+        label={measureShape.label}
+        class="px-4 py-3"
+      />
+    {/each}
+  </div>
+</StyledCard>
